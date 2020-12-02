e8210901ef6e63e4d90ba305c81468f143febeb0
Akhil Gautam <akhilgautam123@gmail.com>
2020-12-03 01:38:47 +0530

    Added basic dropdown component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './assets/main.scss';
-import Badge from '../lib/components/Pill/Pill';
+import { Button, DropdownItem, Dropdown } from '../lib/components';
 
 const wrapper = document.getElementById('root');
 
 const App = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <Badge type="success">cool</Badge>
+      <div className="relative">
+        <Button className="w-auto" onClick={handleOpen}>
+          Open Dropdown&nbsp;&#8595;
+        </Button>
+
+        <Dropdown isOpen={open} onClose={handleClose}>
+          <DropdownItem>
+            <div>Notifications</div>
+            <div>3</div>
+          </DropdownItem>
+          <DropdownItem>
+            <div>Friends</div>
+          </DropdownItem>
+          <DropdownItem>
+            <div>Settings</div>
+          </DropdownItem>
+        </Dropdown>
+      </div>
     </div>
   );
 };
