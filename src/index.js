e8e2fff11c992071a453820f4ef3543697ed6e3d
Akhil Gautam <akhilgautam123@gmail.com>
2020-12-02 11:31:43 +0530

    Added context and renamed badge to pill

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './assets/main.scss';
-import Badge from '../lib/components/Badge/Badge';
+import Badge from '../lib/components/Pill/Pill';
 
 const wrapper = document.getElementById('root');
 
 const App = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <Badge type="primary">cool</Badge>
+      <Badge type="success">cool</Badge>
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  // ...
+  variants: {
+    extend: {
+      display: ['group-hover'],
+    }
+  },
+}
