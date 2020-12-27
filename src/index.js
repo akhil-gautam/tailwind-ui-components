c2e58aace2094e2aaef5b2a9ef185841a786398a
Akhil Gautam <akhilgautam123@gmail.com>
2020-12-27 19:22:47 +0530

    Added Modal component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import './assets/main.scss';
-import { TextInput } from '../lib/components';
+import { Modal, Button } from '../lib/components';
 
 const wrapper = document.getElementById('root');
 
 const App = () => {
+  const [isOpen, setopen] = useState(true);
+  const onClose = () => setopen(false);
+
   return (
     <div className="flex justify-center items-center w-full h-full px-32">
-      <TextInput className="w-2/3" valid/>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <header>
+          <h4 className="font-bold text-lg mb-4">I'm a beautifull header</h4>
+        </header>
+        <div>This is a body element</div>
+        <footer>
+          <hr className="my-4"/>
+          <div className="flex justify-end space-x-4">
+          <Button />
+          <Button />
+          <Button />
+          </div>
+
+        </footer>
+      </Modal>
     </div>
   );
 };
