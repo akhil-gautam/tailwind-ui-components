6d5e5f4f4834160206007fa7e7492bdcadd10109
Akhil Gautam <akhilgautam123@gmail.com>
2021-01-06 11:48:37 +0530

    Added List component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import './assets/main.scss';
-import { Table, Thead, Th, Tr, Tbody, Td, Tfoot } from '../lib/components';
+import { List, ListItem } from '../lib/components';
 
 const wrapper = document.getElementById('root');
 
@@ -11,56 +11,16 @@ const App = () => {
   const onClose = () => setopen(false);
 
   return (
-    <div className='flex justify-center items-center w-full h-full px-32 bg-purple-600'>
-      <>
-        <Table>
-          <Thead>
-            <Tr>
-              <Th>Name</Th>
-              <Th>Roll</Th>
-              <Th>Marks</Th>
-              <Th>Passed</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td>Akhil</Td>
-              <Td>12</Td>
-              <Td>56</Td>
-              <Td>Yes</Td>
-            </Tr>
-            <Tr>
-              <Td>Akhil</Td>
-              <Td>12</Td>
-              <Td>56</Td>
-              <Td>Yes</Td>
-            </Tr>
-            <Tr>
-              <Td>Akhil</Td>
-              <Td>12</Td>
-              <Td>56</Td>
-              <Td>Yes</Td>
-            </Tr>
-            <Tr>
-              <Td>Akhil</Td>
-              <Td>12</Td>
-              <Td>56</Td>
-              <Td>Yes</Td>
-            </Tr>
-            <Tr>
-              <Td>Akhil</Td>
-              <Td>12</Td>
-              <Td>56</Td>
-              <Td>Yes</Td>
-            </Tr>
-          </Tbody>
-          <Tfoot>
-            <Tr>
-              <Td colSpan={4}>This is a footer</Td>
-            </Tr>
-          </Tfoot>
-        </Table>
-      </>
+    <div className='flex justify-center items-center w-full h-full px-32 bg-amber-200'>
+      <List header="This marks the heading">
+        <ListItem>This is a beautiful text.</ListItem>
+        <ListItem>This is a beautiful text.</ListItem>
+        <ListItem>This is a beautiful text.</ListItem>
+        <ListItem>This is a beautiful text.</ListItem>
+        <ListItem>This is a beautiful text.</ListItem>
+        <ListItem>This is a beautiful text.</ListItem>
+        <ListItem>This is a beautiful text.</ListItem>
+      </List>
     </div>
   );
 };
