76e297160c083fe5d1c9f6e401f1a780eafc7260
Akhil Gautam <akhilgautam123@gmail.com>
2021-01-09 23:15:22 +0530

    Added Icon support to button component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import './assets/main.scss';
-import { Alert, ListItem, List, Avatar } from '../lib/components';
+import { Alert, ListItem, List, Avatar, Button } from '../lib/components';
 
 const wrapper = document.getElementById('root');
 
@@ -19,7 +19,10 @@ const App = () => {
       <Alert onClose={() => alert('hello')} variant='filled' type='error'>
         This is a message for you.
       </Alert>
-      <Avatar src='https://bit.ly/2Lvc3a1' size='small'/>
+      <Avatar src='https://bit.ly/2Lvc3a1' size='small' />
+      <Button LeftIcon={BellIcon} type='primary'>
+        Submit
+      </Button>
       <List>
         <ListItem>This is a beautiful text.</ListItem>
         <ListItem>This is a beautiful text.</ListItem>
@@ -32,5 +35,21 @@ const App = () => {
     </div>
   );
 };
+
+const BellIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={2}
+      d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'
+    />
+  </svg>
+);
 
 wrapper ? ReactDOM.render(<App />, wrapper) : false;
